88997d1572ccef74c5b2fd9a8a14cde17dd8d1e2
harsh_nagalla <harsh_nagalla@singaporeair.com.sg>
2021-03-15 15:06:26 +0800

    Add mockdata
    Added mock data for artists thumbnails and directory

---
+++ src/mockData/mockArtistDirectory.js
@@ -0,0 +1,28 @@
+const mockArtistDirectory = [
+	{
+		index: 'A',
+		creators: ['Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black'],
+	},
+	{
+		index: 'B',
+		creators: ['Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black'],
+	},
+	{
+		index: 'C',
+		creators: ['Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black'],
+	},
+	{
+		index: 'D',
+		creators: ['Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black'],
+	},
+	{
+		index: 'E',
+		creators: ['Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black'],
+	},
+	{
+		index: 'F',
+		creators: ['Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black', 'Adam Black'],
+	},
+];
+
+export default mockArtistDirectory;
---
+++ src/mockData/mockArtistList.js
@@ -0,0 +1,32 @@
+const mockArtistList = [
+	{
+		name: 'Adam Neely',
+		profileImage: require('../assets/adam.jpeg'),
+	},
+	{
+		name: 'Alesso',
+		profileImage: require('../assets/alesso.jpg'),
+	},
+	{
+		name: 'Armin',
+		profileImage: require('../assets/armin.jpg'),
+	},
+	{
+		name: 'Avicci',
+		profileImage: require('../assets/avicci.png'),
+	},
+	{
+		name: 'Daft Punk',
+		profileImage: require('../assets/daftpunk.jpg'),
+	},
+	{
+		name: 'Hardwell',
+		profileImage: require('../assets/hardwell.jpeg'),
+	},
+	{
+		name: 'Paul',
+		profileImage: require('../assets/paul.jpg'),
+	},
+];
+
+export default mockArtistList;
